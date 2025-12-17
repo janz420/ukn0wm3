c253cfe752f6fb11f767d22028ee91565de23fec
0x <58542747+janz420@users.noreply.github.com>
2025-12-17 11:13:58 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -13,7 +13,7 @@ banner
 modpeson(){
 settings put global airplane_mode_on 1
 am broadcast -a android.intent.action.AIRPLANE_MODE
-settings put global airplane_mode_on 1 && am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
+su -c settings put global airplane_mode_on 1 && am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
 }
 delete(){
 am force-stop com.fufufu.katrina.backup
